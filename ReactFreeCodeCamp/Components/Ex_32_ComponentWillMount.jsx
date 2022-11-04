75b2130f527c98a23b22c8550db02befafd75650
GaboPosso <gabopos@gmail.com>
2022-11-03 23:20:38 -0500

    Upload exercises 32 - 33 of LyfeCycle Methods of React

---
+++ ReactFreeCodeCamp/Components/Ex_32_ComponentWillMount.jsx
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+    console.log('This will be showed in the console.');
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
